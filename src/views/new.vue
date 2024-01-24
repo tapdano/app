08bc3eb87ed25bfc416cd6f2c6b65509efce2484
Lucas Boemeke <boemekeld@gmail.com>
2024-01-23 22:49:27 -0300

    up

--- src/views/new.vue
+++ src/views/new.vue
@@ -11,23 +11,66 @@
 
     <ion-content :fullscreen="true">
       <div id="container">
-        <p>New</p>
+        <form @submit.prevent="handleSubmit">
+          <ion-item>
+            <ion-label position="floating">Name</ion-label>
+            <ion-input placeholder="My Investments"></ion-input>
+          </ion-item>
+
+          <div id="advanced-options" @click="toggleAdvancedOptions">
+            <span class="link-style">Advanced Options</span>
+          </div>
+          <div v-show="showAdvancedOptions">
+            <ion-item>
+              <ion-label>Type</ion-label>
+              <ion-select value="nfc-wallet" placeholder="Select One">
+                <ion-select-option value="nfc-wallet">NFC Wallet</ion-select-option>
+                <ion-select-option value="nfc-authentication">NFC Authentication</ion-select-option>
+              </ion-select>
+            </ion-item>
+          </div>
+
+          <ion-button id="submit-button" expand="block" type="submit">Create a new wallet</ion-button>
+        </form>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/vue';
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonInput, IonSelect, IonSelectOption, IonButton } from '@ionic/vue';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const showAdvancedOptions = ref(false);
+
+const toggleAdvancedOptions = () => {
+  showAdvancedOptions.value = !showAdvancedOptions.value;
+};
+
+const handleSubmit = () => {
+  router.push('/wallet/main');
+};
 </script>
 
 <style scoped>
 #container {
-  text-align: center;
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+  margin: 20px;
+}
+
+#advanced-options {
+  margin: 20px 0;
+  align-items: center;
+  font-size: 12px;
+  text-align: right;
+}
+
+.link-style {
+  text-decoration: underline;
+}
+
+#submit-button {
+  margin-top: 20px;
 }
 </style>
