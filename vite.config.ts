bb1ff74cc4557eff9e195cac2db5d5c96c08167e
Lucas Boemeke <lucas@blueshift.cc>
2024-01-26 03:07:28 -0300

    up

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,18 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
 import wasm from 'vite-plugin-wasm'
+import topLevelAwait from "vite-plugin-top-level-await"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     wasm(),
     vue(),
-    legacy()
+    legacy(),
+    topLevelAwait({
+      promiseExportName: "__tla",
+      promiseImportName: i => `__tla_${i}`
+    })
   ],
   resolve: {
     alias: {
