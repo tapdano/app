a91f51360a4978b51fc27235f1d15d7a14f39b18
Lucas Boemeke <boemekeld@gmail.com>
2024-02-08 04:38:48 -0300

    pwa test

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,34 @@
+const CACHE_NAME = 'dynamic-v1';
+
 self.addEventListener('install', (event) => {
   console.log('Service Worker installing.');
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Service Worker activated.');
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('Service Worker: Deleting old cache:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log('Fetching:', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).then(fetchRes => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request.url, fetchRes.clone());
+          return fetchRes;
+        });
+      });
+    })
+  );
 });
