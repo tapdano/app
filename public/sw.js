0a85850751ba144eb1513e1a7dfe10b2484fdb92
Lucas Boemeke <boemekeld@gmail.com>
2024-02-08 05:02:01 -0300

    bypass external requests on sw cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dynamic-v1';
+const CACHE_NAME = 'dynamic-v2';
 
 self.addEventListener('install', (event) => {
   console.log('Service Worker installing.');
@@ -21,14 +21,18 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request).then(fetchRes => {
-        return caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request.url, fetchRes.clone());
-          return fetchRes;
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request).then(fetchRes => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request.url, fetchRes.clone());
+            return fetchRes;
+          });
         });
-      });
-    })
-  );
+      })
+    );
+  } else {
+    event.respondWith(fetch(event.request));
+  }
 });
