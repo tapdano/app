7afecae0f1229045cd0108171292ebf13a8a2f0c
Lucas Boemeke <boemekeld@gmail.com>
2024-02-08 04:16:39 -0300

    pwa implement

--- src/main.ts
+++ src/main.ts
@@ -35,3 +35,13 @@ const app = createApp(App)
 router.isReady().then(() => {
   app.mount('#app');
 });
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }, (err) => {
+      console.log('Service Worker registration failed:', err);
+    });
+  });
+}
