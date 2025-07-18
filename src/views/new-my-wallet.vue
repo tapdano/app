749280a7ee4b6cead210b53a5710a67d1258890c
Lucas Boemeke <boemekeld@gmail.com>
2025-07-18 05:42:21 -0300

    fix build

--- src/views/new-my-wallet.vue
+++ src/views/new-my-wallet.vue
@@ -33,9 +33,8 @@ import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { Storage } from '@ionic/storage';
 import { createWallet, entropyToMnemonic } from '@/utils/CryptoUtils';
-import { TapDanoService } from 'tapdano';
+import { TagParser, TapDanoService } from 'tapdano';
 import NFCModal from '@/components/NFCModal.vue';
-import { addMyWallet } from '@/utils/StorageUtils';
 
 const props = defineProps({
   route: String
@@ -48,6 +47,35 @@ storage.create();
 const walletName = ref('');
 const walletRecoveryPhrase = ref('');
 const nfcModal = ref<InstanceType<typeof NFCModal> | null>(null);
+
+async function addMyWallet(tag: TagParser, name: string | undefined) {
+  const storage = new Storage();
+  await storage.create();
+  
+  const wallets = (await storage.get('my-wallets')) || [];
+
+  const mnemonic = entropyToMnemonic(tag.PrivateKey as string);
+  const cryptoWallet = await createWallet(mnemonic);
+
+  const index = wallets.findIndex((item: any) => {
+    return item.baseAddr == cryptoWallet.baseAddr;
+  });
+
+  if (index != -1) {
+    await storage.set('currentMyWallet', index);
+    return;
+  }
+  
+  const newIndex = wallets.length;
+  wallets.push({
+    name: name || `TapWallet #${Math.floor(Math.random() * 1000000).toString().padStart(6, '0')}`,
+    tag,
+    ...cryptoWallet,
+  });
+
+  await storage.set('my-wallets', wallets);
+  await storage.set('currentMyWallet', newIndex);
+}
 
 const handleSubmit = async () => {
   if (!nfcModal.value) return;
